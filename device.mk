ec8a5fbbd956a990a3d5b5709537a6c78d8e5c80
guaiyihu <guaiyihu@foxmail.com>
2018-09-20 09:55:47 +0800

    r5x: Remove non-existent android.hardware.audio.effect@2.0-service
    Change-Id: Iea3cb72c886470b5dc4a51365db4bd151601360c
    (cherry picked from commit e3d3c53457dbe96cef663e64d63b1d3f31a8846f)

--- device.mk
+++ device.mk
@@ -22,7 +22,6 @@ PRODUCT_PACKAGES += \
 PRODUCT_PACKAGES += \
     android.hardware.audio@5.0-impl \
     android.hardware.audio.effect@5.0-impl \
-    android.hardware.audio.effect@2.0-service \
     android.hardware.soundtrigger@2.2-impl \
     android.hardware.audio.service \
     audio.primary.trinket \
