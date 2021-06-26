7473d44801e30e896d1888c33edb6a3588366760
Albert I <kras@raphielgang.org>
2021-06-26 15:30:31 +0800

    r5x: config: Inherit several Android Go configurations
    * Beneficial for everyone, even on non-Go devices.
    
    Change-Id: I088fd00500ce10df5f413cb762aaed2ec00f5dfc
    Signed-off-by: Christopher <mizdrake7@gmail.com>

--- device.mk
+++ device.mk
@@ -468,6 +468,10 @@ PRODUCT_PACKAGES += \
     libipanat \
     liboffloadhal
 
+# Improve performance
+PRODUCT_USE_PROFILE_FOR_BOOT_IMAGE := true
+PRODUCT_DEX_PREOPT_BOOT_IMAGE_PROFILE_LOCATION := frameworks/base/config/boot-image-profile.txt
+
 # Trust
 PRODUCT_PACKAGES += \
     vendor.lineage.trust@1.0-service
