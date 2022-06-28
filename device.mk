857c1fcb289867b9a2940a29b341777f7b3095b9
Alexander Koskovich <akoskovich@pm.me>
2022-06-28 06:15:16 -0700

    r5x: Drop vendor com.qualcomm.qti.bluetooth_audio@1.0
     * Nothing references it.
    
    Change-Id: I35703759dc7d3cf711a84ff2c5d2abc14bd156cf

--- device.mk
+++ device.mk
@@ -63,7 +63,6 @@ PRODUCT_PACKAGES += \
     audio.bluetooth.default \
     android.hardware.bluetooth@1.0.vendor \
     android.hardware.bluetooth.audio-impl \
-    com.qualcomm.qti.bluetooth_audio@1.0.vendor \
     libldacBT_bco \
     vendor.qti.hardware.bluetooth_audio@2.1.vendor \
     vendor.qti.hardware.btconfigstore@1.0.vendor \
