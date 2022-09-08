4d2db95eeefbf92c8a8d6536501c1d3aab70cf51
takumi021 <omrameshpatel@gmail.com>
2022-09-08 16:58:03 +0000

    r5x: Update Thermal Service from RMX2050_11_C.10
    - Drop Ginkgo Thermal Service
    
    Co-authored-by: TheMalachite <eliasgheeraert@gmail.com>
    Signed-off-by: Amritorock <amritokarmokar5i@gmail.com>

--- device.mk
+++ device.mk
@@ -468,12 +468,6 @@ PRODUCT_DEX_PREOPT_BOOT_IMAGE_PROFILE_LOCATION := frameworks/base/config/boot-im
 PRODUCT_PACKAGES += \
     android.hardware.thermal@2.0-service.mock
 
-PRODUCT_COPY_FILES += \
-    $(LOCAL_PATH)/configs/thermal/thermal-engine.conf:$(TARGET_COPY_OUT_VENDOR)/etc/thermal-engine.conf \
-    $(LOCAL_PATH)/configs/thermal/thermal-engine-camera.conf:$(TARGET_COPY_OUT_VENDOR)/etc/thermal-engine-camera.conf \
-    $(LOCAL_PATH)/configs/thermal/thermal-engine-map.conf:$(TARGET_COPY_OUT_VENDOR)/etc/thermal-engine-map.conf \
-    $(LOCAL_PATH)/configs/thermal/thermal-engine-normal.conf:$(TARGET_COPY_OUT_VENDOR)/etc/thermal-engine-normal.conf
-
 # USB
 PRODUCT_PACKAGES += \
     android.hardware.usb@1.0-service
