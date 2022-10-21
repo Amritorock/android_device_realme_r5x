c439da70c79b54d8e2da7b6c83bb1157093220be
Jiakai Zhang <jiakaiz@google.com>
2022-10-21 20:07:17 +0000

    r5x: Preopt SystemUI for AOSP builds

--- device.mk
+++ device.mk
@@ -155,6 +155,10 @@ PRODUCT_COPY_FILES += \
     frameworks/native/data/etc/android.hardware.vulkan.level-1.xml:$(TARGET_COPY_OUT_VENDOR)/etc/permissions/android.hardware.vulkan.level-1.xml \
     frameworks/native/data/etc/android.hardware.vulkan.version-1_1.xml:$(TARGET_COPY_OUT_VENDOR)/etc/permissions/android.hardware.vulkan.version-1_1.xml
 
+# Dexpreopt
+PRODUCT_DEXPREOPT_SPEED_APPS += \
+    SystemUI
+
 # DSP
 PRODUCT_PACKAGES += \
     vendor.qti.hardware.dsp@1.0
