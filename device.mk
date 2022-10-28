fa8a8bf3055099c2c20aaf27c07b7dbcf1764f8d
Gabriel Biren <gbiren@google.com>
2022-10-28 18:54:04 +0000

    r5x: Use AIDL WiFi service
    Bug: 205044134
    Test: Presubmit tests
    Change-Id: Icdaa448e2ed59b638056593adb4bcd769f34220d

--- device.mk
+++ device.mk
@@ -485,7 +485,7 @@ $(call inherit-product, vendor/qcom/opensource/vibrator/vibrator-vendor-product.
 
 # Wifi
 PRODUCT_PACKAGES += \
-    android.hardware.wifi@1.0-service \
+    android.hardware.wifi-service \
     hostapd \
     libwifi-hal-ctrl \
     libwifi-hal-qcom \
