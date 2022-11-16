77ccbdd419caec8a88ce631d60116a734cf9568e
AmritoRock <amritokarmokar5i@gmail.com>
2022-11-16 01:14:07 +0000

    r5x: Init riceDroid

--- lineage_r5x.mk
+++ lineage_r5x.mk
@@ -1,5 +1,5 @@
 #
-# Copyright (C) 2022 Project Elixir
+# Copyright (C) 2022 Project LineageOs
 #
 # SPDX-License-Identifier: Apache-2.0
 #
@@ -9,20 +9,19 @@ $(call inherit-product, $(SRC_TARGET_DIR)/product/core_64_bit.mk)
 $(call inherit-product, $(SRC_TARGET_DIR)/product/full_base_telephony.mk)
 $(call inherit-product, $(SRC_TARGET_DIR)/product/product_launched_with_p.mk)
 
-# Inherit some common Project-Elixir stuff
-$(call inherit-product, vendor/aosp/config/common_full_phone.mk)
+# Inherit some common riceDroid stuff
+$(call inherit-product, vendor/lineage/config/common_full_phone.mk)
 
 # Inherit from r5x device
 $(call inherit-product, $(LOCAL_PATH)/device.mk)
 
-TARGET_BOOT_ANIMATION_RES := 720
 TARGET_GAPPS_ARCH := arm64
 TARGET_INCLUDE_LIVE_WALLPAPERS := false
 
 PRODUCT_BRAND := realme
 PRODUCT_DEVICE := r5x
 PRODUCT_MANUFACTURER := realme
-PRODUCT_NAME := aosp_r5x
+PRODUCT_NAME := lineage_r5x
 PRODUCT_MODEL := Realme 5 Series
 
 PRODUCT_GMS_CLIENTID_BASE := android-realme
@@ -34,11 +33,13 @@ PRODUCT_BUILD_PROP_OVERRIDES += \
     PRODUCT_NAME="r5x" \
     PRIVATE_BUILD_DESC="trinket-user 10 QKQ1.200209.002 release-keys"
 
-# Project-Elixir Stuff
-IS_PHONE := true
-ELIXIR_BUILD_TYPE := OFFICIAL
+# riceDroid Stuff
+SUSHI_BOOTANIMATION  := 720
+TARGET_ENABLE_BLUR := true
 TARGET_SUPPORTS_GOOGLE_RECORDER := true
-TARGET_INCLUDE_STOCK_ACORE := false
-TARGET_FACE_UNLOCK_SUPPORTED := true
-TARGET_SUPPORTS_CALL_RECORDING := true
-FORCE_LAWNCHAIR := true
+TARGET_BUILD_GRAPHENEOS_CAMERA := false
+RICE_MAINTAINER := AMRITO
+TARGET_USE_PIXEL_FINGERPRINT := true
+TARGET_OPTOUT_GOOGLE_TELEPHONY := true
+TARGET_BUILD_APERTURE_CAMERA := true
+TARGET_EXCLUDES_AUDIOFX := false
