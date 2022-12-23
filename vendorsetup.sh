3d5116cb84a8eaf01ee24b7940603e9bc108b5ba
Amrito Karmokar <amritokarmokar5i@gmail.com>
2022-12-23 14:53:36 +0600

    r5x: Vendorsetup Don't clone device tree

--- vendorsetup.sh
+++ vendorsetup.sh
@@ -1,6 +1,4 @@
 # clone
-git clone --depth=1 https://github.com/ProjectElixir-Devices/device_realme_r5x -b ELIXIR-13 device/realme/r5x
-
 git clone --depth=1 https://github.com/Amritorock/vendor_realme_r5x -b Trinket vendor/realme/r5x
 
 git clone --depth=1 https://github.com/Amritorock/kernel_realme_r5x -b OSS kernel/realme/r5x
