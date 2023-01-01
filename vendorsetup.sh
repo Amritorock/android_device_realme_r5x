c3225de7535fd160128f917d61b9ed13d468124a
AmritoRock <amritokarmokar5i@gmail.com>
2023-01-01 14:33:17 +0000

    r5: Drop Vendorsetuo.sh [temp]

--- vendorsetup.sh
+++
@@ -1,4 +0,0 @@
-# clone
-git clone --depth=1 https://github.com/Amritorock/vendor_realme_r5x -b Trinket vendor/realme/r5x
-
-git clone --depth=1 https://github.com/Amritorock/kernel_realme_r5x -b OSS kernel/realme/r5x
