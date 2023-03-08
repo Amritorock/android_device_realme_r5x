9827bd7aba44604fa7339c82cb54ce42deb23f2b
ghostrider-reborn <gh0strider.2k18.reborn@gmail.com>
2023-03-08 21:37:52 +0000

    r5x: Build required HIDL libs to vendor
    * they're not a part of vndk (since 31) anymore
     * cleanup camera interfaces while we're at it

--- device.mk
+++ device.mk
@@ -84,17 +84,12 @@ PRODUCT_PACKAGES += \
 # Camera
 PRODUCT_PACKAGES += \
     android.frameworks.sensorservice@1.0.vendor \
-    android.hardware.camera.device@3.4 \
-    android.hardware.camera.device@3.5 \
     android.hardware.camera.provider@2.4-impl \
     android.hardware.camera.provider@2.4-service \
-    android.hardware.camera.provider@2.5 \
-    vendor.qti.hardware.camera.device@1.0 \
+    android.hidl.memory.block@1.0.vendor \
     vendor.qti.hardware.camera.device@1.0.vendor \
     libcamera2ndk_vendor \
-    libstdc++.vendor \
-    libdng_sdk.vendor \
-    Snap
+    libstdc++.vendor
 
 PRODUCT_COPY_FILES += \
     frameworks/native/data/etc/android.hardware.camera.flash-autofocus.xml:$(TARGET_COPY_OUT_VENDOR)/etc/permissions/android.hardware.camera.flash-autofocus.xml \
@@ -117,7 +112,7 @@ PRODUCT_PROPERTY_OVERRIDES += \
 # Display
 PRODUCT_PACKAGES += \
     android.frameworks.displayservice@1.0_32 \
-    android.frameworks.displayservice@1.0 \
+    android.frameworks.displayservice@1.0.vendor \
     android.hardware.graphics.composer@2.4-impl \
     android.hardware.graphics.composer@2.4-service \
     android.hardware.graphics.mapper@3.0-impl-qti-display \
@@ -154,7 +149,8 @@ PRODUCT_PACKAGES += \
 
 # DRM
 PRODUCT_PACKAGES += \
-    android.hardware.drm@1.4-service.clearkey
+    android.hardware.drm@1.4-service.clearkey \
+    android.hardware.drm@1.4.vendor
 
 PRODUCT_PACKAGES += \
     android.hardware.broadcastradio@1.0-impl
