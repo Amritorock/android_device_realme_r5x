e10ecfb268c7d924e1bb3c22516cb38d76fb1102
Bruno Martins <bgcngm@gmail.com>
2024-02-08 17:05:32 +0000

    r5x: Use the common device compatibility matrix from QCOM
    Change-Id: I0c0f3ecddbe6d7b232cc0d13c78ca58094e4e720
    Signed-off-by: kaderbava <ksbava7325@gmail.com>

--- BoardConfig.mk
+++ BoardConfig.mk
@@ -80,7 +80,7 @@ TARGET_USES_HARDWARE_QCOM_GPS := false
 
 # HIDL
 DEVICE_MANIFEST_FILE := $(DEVICE_PATH)/configs/manifests/manifest.xml
-DEVICE_MATRIX_FILE := $(DEVICE_PATH)/configs/manifests/compatibility_matrix.xml
+DEVICE_MATRIX_FILE := hardware/qcom-caf/common/compatibility_matrix.xml
 ODM_MANIFEST_SKUS += nfc_ese
 ODM_MANIFEST_NFC_ESE_FILES := $(DEVICE_PATH)/configs/manifests/manifest_nfc_ese.xml
 
