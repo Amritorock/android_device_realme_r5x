316a3d0cddb456d825696147691cd42573d77f0b
AmritoRock <amritokarmokar5i@gmail.com>
2024-03-20 20:08:17 +0600

    r5x: Init PixelOS

--- aosp_r5x.mk
+++ aosp_r5x.mk
@@ -1,5 +1,5 @@
 #
-# Copyright (C) 2022 The Project Elixir
+# Copyright (C) 2022 The PixelOS
 #
 # SPDX-License-Identifier: Apache-2.0
 #
@@ -8,7 +8,7 @@
 $(call inherit-product, $(SRC_TARGET_DIR)/product/core_64_bit.mk)
 $(call inherit-product, $(SRC_TARGET_DIR)/product/full_base_telephony.mk)
 
-# Inherit some common Project-Elixir stuff
+# Inherit some common PixelOS stuff
 $(call inherit-product, vendor/aosp/config/common_full_phone.mk)
 
 # Inherit from r5x device
@@ -25,19 +25,3 @@ PRODUCT_NAME := aosp_r5x
 PRODUCT_MODEL := Realme 5 Series
 
 PRODUCT_GMS_CLIENTID_BASE := android-oppo
-
-TARGET_VENDOR_PRODUCT_NAME := r5x
-TARGET_VENDOR_DEVICE_NAME := r5x
-
-PRODUCT_BUILD_PROP_OVERRIDES += \
-    PRODUCT_NAME="r5x" \
-    PRIVATE_BUILD_DESC="trinket-user 10 QKQ1.200209.002 release-keys"
-
-# Project-Elixir Stuff
-IS_PHONE := true
-ELIXIR_BUILD_TYPE := OFFICIAL
-TARGET_SUPPORTS_GOOGLE_RECORDER := true
-TARGET_INCLUDE_STOCK_ACORE := false
-TARGET_FACE_UNLOCK_SUPPORTED := true
-TARGET_SUPPORTS_CALL_RECORDING := true
-TARGET_BUILD_APERTURE_CAMERA := true
