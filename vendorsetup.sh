d38b2e536340d7822989f5eff718292ae25ea57c
AmritoRock <amritokarmokar5i@gmail.com>
2024-03-23 20:29:58 +0600

    r5x: Switch to ghost kernel

--- vendorsetup.sh
+++ vendorsetup.sh
@@ -2,7 +2,7 @@
 git clone --depth=1 https://github.com/Amritorock/vendor_realme_r5x -b Udc vendor/realme/r5x
 
 # Kernel Tree
-git clone --depth=1 https://github.com/Amritorock/kernel_realme_r5x -b OSS kernel/realme/r5x
+git clone --depth=1 https://github.com/Amritorock/kernel_realme_r5t -b main kernel/realme/r5x
 
 # Viper Fx
 git clone --depth=1 https://github.com/Amritorock/packages_apps_ViPER4AndroidFX packages/apps/ViPER4AndroidFX
