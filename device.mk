21429db48f9d45e10dba714e17ddf049e0f36d19
Roker2 (Dmitry Minko) <dimaminko2000@gmail.com>
2024-06-08 14:14:03 +0200

    r5x: Fix building SettingsProviderOverlayR5x

--- device.mk
+++ device.mk
@@ -376,7 +376,7 @@ PRODUCT_PACKAGES += \
 PRODUCT_PACKAGES += \
     CarrierConfigOverlayR5x \
     TelephonyOverlayR5x \
-    SettingsProviderOverayR5x \
+    SettingsProviderOverlayR5x \
     SettingsOverlayR5x \
     SystemUIOverlayR5x \
     WifiOverlayR5x \
