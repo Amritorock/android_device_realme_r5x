1cb0168b16aa4ac4a1fe9e570be5bbb4a9c2d3b4
Yumi Yukimura <me.cafebabe@gmail.com>
2024-06-19 11:26:40 +0800

    r5x: Build AOSP Radio HIDL->AIDL Wrapper
    Change-Id: Iaeb8d37f4a7f02e6e812bc4da1b413c9a6493f8f

--- device.mk
+++ device.mk
@@ -300,6 +300,7 @@ PRODUCT_COPY_FILES += \
 
 # Telephony
 PRODUCT_PACKAGES += \
+    android.hardware.radio-service.compat \
     ims-ext-common \
     ims_ext_common.xml \
     qti-telephony-hidl-wrapper \
