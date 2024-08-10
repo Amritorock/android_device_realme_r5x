18940323160511a1724d475904c387adc2931c02
adi8900 <adrianszymanski242@gmail.com>
2024-08-10 23:25:01 +0600

    r5x: Update QMI/Radio/IMS system Blobs from LA.QSSI.14.0.r1-15800-qssi.0
    - CarrierConfig from LA.QSSI.14.0.r1-12000-qssi.0
    
    Signed-off-by: Amritorock <amritokarmokar5i@gmail.com>

--- device.mk
+++ device.mk
@@ -396,8 +396,7 @@ TARGET_BOARD_PLATFORM := trinket
 # QTI
 PRODUCT_COPY_FILES += \
     $(LOCAL_PATH)/configs/qti_whitelist.xml:system/etc/sysconfig/qti_whitelist.xml \
-    $(LOCAL_PATH)/configs/privapp-permissions-qti.xml:system/etc/permissions/privapp-permissions-qti.xml \
-    $(LOCAL_PATH)/configs/telephony_product_privapp-permissions-qti.xml:$(TARGET_COPY_OUT_PRODUCT)/etc/permissions/telephony_product_privapp-permissions-qti.xml
+    $(LOCAL_PATH)/configs/privapp-permissions-qti.xml:system/etc/permissions/privapp-permissions-qti.xml
 
 # RealmeParts
 PRODUCT_PACKAGES += \
