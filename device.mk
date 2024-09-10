48846850d92ab60cd46215da0d43ec7174000578
Michael Bestas <mkbestas@gmail.com>
2024-09-11 00:04:13 +0300

    r5x: Shim widevine with libcrypto_shim
    The following change
    https://github.com/google/boringssl/commit/e202e51cb0912f36dafbd2e67cf04d6ec82f3180
    removed symbols that our libs require.
    
    Change-Id: I1325c34ecec3d339dd68c665010ecb36fa2e6a29
    Signed-off-by: adi8900 <adrianszymanski242@gmail.com>

--- device.mk
+++ device.mk
@@ -125,7 +125,8 @@ PRODUCT_COPY_FILES += \
 # DRM
 PRODUCT_PACKAGES += \
     android.hardware.drm-service.clearkey \
-    android.hardware.drm@1.4.vendor
+    android.hardware.drm@1.4.vendor \
+    libcrypto_shim.vendor
 
 PRODUCT_PACKAGES += \
     android.hardware.broadcastradio@1.0-impl
--- extract-files.sh
+++ extract-files.sh
@@ -61,6 +61,7 @@ function blob_fixup() {
             ;;
         vendor/lib64/libwvhidl.so)
             "${PATCHELF}" --replace-needed "libprotobuf-cpp-lite-3.9.1.so" "libprotobuf-cpp-full-3.9.1.so" "${2}"
+            "${PATCHELF}" --add-needed "libcrypto_shim.so" "${2}"
             ;;
        vendor/lib/libsnsdiaglog.so)
             "${PATCHELF}" --replace-needed "libprotobuf-cpp-lite-3.9.1.so" "libprotobuf-cpp-full-3.9.1.so" "${2}"
