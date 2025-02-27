58f3fb306e954ecb1cdb3253da0b6c426afa3c6d
AmritoRock <amritokarmokar5i@gmail.com>
2025-02-27 09:06:10 +0600

    r5x: AxionAOSP Bringup
    Signed-off-by: AmritoRock <amritokarmokar5i@gmail.com>

--- device.mk
+++ device.mk
@@ -71,6 +71,10 @@ PRODUCT_COPY_FILES += \
     frameworks/native/data/etc/android.hardware.bluetooth_le.xml:$(TARGET_COPY_OUT_VENDOR)/etc/permissions/android.hardware.bluetooth_le.xml \
     frameworks/native/data/etc/android.hardware.bluetooth.xml:$(TARGET_COPY_OUT_VENDOR)/etc/permissions/android.hardware.bluetooth.xml
 
+# Boot animation
+TARGET_SCREEN_HEIGHT := 1600
+TARGET_SCREEN_WIDTH := 720
+
 # Camera
 PRODUCT_PACKAGES += \
     android.frameworks.sensorservice@1.0.vendor \
