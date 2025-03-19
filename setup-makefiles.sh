83f9b81a2b7e1deb8b1486a26a5edbf05633e9b0
AmritoRock <amritokarmokar5i@gmail.com>
2025-03-20 02:28:41 +0600

    r5x: switch to source built `vendor.oppo.hardware.biometrics.fingerprint@2.1`
    Signed-off-by: AmritoRock <amritokarmokar5i@gmail.com>

--- setup-makefiles.sh
+++ setup-makefiles.sh
@@ -50,7 +50,6 @@ function lib_to_package_fixup_vendor_variants() {
             com.qualcomm.qti.imscmservice@2.2 | \
             com.qualcomm.qti.uceservice@2.0 | \
             com.qualcomm.qti.uceservice@2.1 | \
-            vendor.oppo.hardware.biometrics.fingerprint@2.1 | \
             vendor.qti.hardware.data.cne.internal.api@1.0 | \
             vendor.qti.hardware.data.cne.internal.constants@1.0 | \
             vendor.qti.hardware.data.cne.internal.server@1.0 | \
