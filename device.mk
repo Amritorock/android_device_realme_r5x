5b7d0e3a5b43c02fbae5080d15f11dbb34efbccc
AmritoRock <amritokarmokar5i@gmail.com>
2025-06-16 02:25:05 +0600

    r5x: Checkout Bluetooth and Ant+ flags from `sm6150-common`
    - https://github.com/Amritorock/android_device_realme_r5x/commit/da63075883d5d98e4458055dade2e2c9ad5bf3f1
    - https://github.com/Amritorock/android_device_realme_r5x/commit/2b05b40358df15bf911e27cecf4899238ce47c6a
    - missed while updateing blobs checkout props/overlay/flags
    
    Signed-off-by: Amritorock <amritokarmokar5i@gmail.com>

--- device.mk
+++ device.mk
@@ -17,7 +17,7 @@ $(call inherit-product, $(SRC_TARGET_DIR)/product/emulated_storage.mk)
 
 # ANT+
 PRODUCT_PACKAGES += \
-    com.dsi.ant@1.0.vendor
+    AntHalService-Soong
 
 # Audio
 PRODUCT_PACKAGES += \
@@ -61,12 +61,8 @@ PRODUCT_COPY_FILES += \
 
 # Bluetooth
 PRODUCT_PACKAGES += \
-    audio.bluetooth.default \
-    android.hardware.bluetooth@1.0.vendor \
     android.hardware.bluetooth.audio-impl \
-    vendor.qti.hardware.bluetooth_audio@2.1.vendor \
-    vendor.qti.hardware.btconfigstore@1.0.vendor \
-    vendor.qti.hardware.btconfigstore@2.0.vendor
+    audio.bluetooth.default
 
 PRODUCT_COPY_FILES += \
     frameworks/native/data/etc/android.hardware.bluetooth_le.xml:$(TARGET_COPY_OUT_VENDOR)/etc/permissions/android.hardware.bluetooth_le.xml \
