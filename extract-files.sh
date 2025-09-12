0b4d9970ec3415c746ca0ec95a83a7c535358ce0
Debayan Kar <kardebayan3@gmail.com>
2025-09-12 15:08:18 +0530

    r5x: Dont patch sensor blobs
    - Also reorder blobs
    
    Signed-off-by: Debayan Kar <kardebayan3@gmail.com>

--- extract-files.sh
+++ extract-files.sh
@@ -61,10 +61,6 @@ function blob_fixup() {
             [ "$2" = "" ] && return 0
             "${PATCHELF}" --add-needed "libcrypto_shim.so" "${2}"
             ;;
-       vendor/lib/libsnsdiaglog.so|vendor/lib/libsnsapi.so|vendor/lib64/libsnsapi.so)
-            [ "$2" = "" ] && return 0
-            "${PATCHELF}" --replace-needed "libprotobuf-cpp-lite-3.9.1.so" "libprotobuf-cpp-full-3.9.1.so" "${2}"
-            ;;
        vendor/lib/libOPPORectify.so|vendor/lib/libarcsoft_beautyshot_lite_image.so|vendor/lib/libarcsoft_hdr_couple_api.so|vendor/lib/libarcsoft_high_dynamic_range_couple.so|vendor/lib/libarcsoft_picauto.so|vendor/lib/libblur_channel.so|vendor/lib/libthread_blur.so|vendor/lib/libdepthmap.so)
             [ "$2" = "" ] && return 0
             "${PATCHELF}" --replace-needed "libstdc++.so" "libstdc++_vendor.so" "${2}"
