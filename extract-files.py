c69ef5ed0be524428d58389bc7d8bc1a90dd2234
Amrito Karmokar <amritokarmokar5i@gmail.com>
2025-09-13 19:32:30 +0000

    r5x: Patch Widevine and Codec C2 for cgroup migration
    Signed-off-by: Amrito Karmokar <amritokarmokar5i@gmail.com>

--- extract-files.py
+++ extract-files.py
@@ -55,7 +55,9 @@ blob_fixups: blob_fixups_user_type = {
     ('vendor/lib/libOPPORectify.so', 'vendor/lib/libarcsoft_beautyshot_lite_image.so', 'vendor/lib/libarcsoft_hdr_couple_api.so', 'vendor/lib/libarcsoft_high_dynamic_range_couple.so', 'vendor/lib/libarcsoft_picauto.so', 'vendor/lib/libblur_channel.so', 'vendor/lib/libthread_blur.so', 'vendor/lib/libdepthmap.so'): blob_fixup()
         .replace_needed('libstdc++.so', 'libstdc++_vendor.so'),
     'vendor/lib64/libwvhidl.so': blob_fixup()
-        .add_needed('libcrypto_shim.so')
+        .add_needed('libcrypto_shim.so'),
+    ('vendor/etc/init/android.hardware.drm@1.3-service.widevine.rc', 'vendor/etc/init/vendor.qti.media.c2@1.0-service.rc'): blob_fixup()
+        .regex_replace(r'writepid /dev/cpuset/foreground/tasks', 'task_profiles ProcessCapacityHigh'),
 }  # fmt: skip
 
 module = ExtractUtilsModule(
