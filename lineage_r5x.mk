8e7c4b8e147558b77a755fc8beaca5c8233fdfdd
AmritoRock <amritokarmokar5i@gmail.com>
2025-11-04 02:33:11 +0600

    r5x: Setup GPU_FREQS_PATH and PERF_DEFAULT_GOV values
    Signed-off-by: AmritoRock <amritokarmokar5i@gmail.com>

--- lineage_r5x.mk
+++ lineage_r5x.mk
@@ -30,3 +30,7 @@ AXION_PROCESSOR := Snapdragon_665™
 TARGET_ENABLE_BLUR := true
 TARGET_PREBUILT_BCR := false
 TORCH_STR_SUPPORTED := true
+PERF_GOV_SUPPORTED := true
+PERF_DEFAULT_GOV := schedutil
+GPU_FREQS_PATH := /sys/class/kgsl/kgsl-3d0/devfreq/available_frequencies
+GPU_MIN_FREQ_PATH := /sys/class/kgsl/kgsl-3d0/devfreq/min_freq
