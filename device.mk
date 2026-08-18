22a1ab5aea7de148c0668157b687b2817d0e2fe0
AmritoRock <amritokarmokar5i@gmail.com>
2026-08-18 21:13:06 +0600

    r5x: Copy `taskprofile and cgroup` json from source
    Signed-off-by: AmritoRock <amritokarmokar5i@gmail.com>

--- device.mk
+++ device.mk
@@ -297,8 +297,8 @@ PRODUCT_SOONG_NAMESPACES += \
 
 # Task Profiles
 PRODUCT_COPY_FILES += \
-    $(LOCAL_PATH)/configs/profiles/task_profiles_30.json:$(TARGET_COPY_OUT_VENDOR)/etc/task_profiles.json \
-    $(LOCAL_PATH)/configs/profiles/cgroups_30.json:$(TARGET_COPY_OUT_VENDOR)/etc/cgroups.json
+    system/core/libprocessgroup/profiles/task_profiles.json:$(TARGET_COPY_OUT_VENDOR)/etc/task_profiles.json \
+    system/core/libprocessgroup/profiles/cgroups.json:$(TARGET_COPY_OUT_VENDOR)/etc/cgroups.json
 
 # Telephony
 PRODUCT_PACKAGES += \
